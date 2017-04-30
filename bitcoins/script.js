5392cc3eece13f97d68c3cb8b629938a22a5a273
Jibril785 <jibril.elboukhari@gmail.com>
2017-04-30 13:46:08 +0200

    MAJ

--- bitcoins/script.js
+++ bitcoins/script.js
@@ -20,6 +20,10 @@
             requeteAPI("https://bitcoin.mubiz.com/peerinfo", "peerinfo");
         }
 
+        function output(inp) {
+    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+}
+
         function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
@@ -38,4 +42,7 @@
         return '<span class="' + cls + '">' + match + '</span>';
     });
 }
+
+output(str);
+output(syntaxHighlight(str));
 
